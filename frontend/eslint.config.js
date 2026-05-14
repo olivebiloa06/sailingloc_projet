05850f3a956fe7d6137ea8123e86b2a7ff2a4f85
Olive <olivebiloa06@gmail.com>
2026-05-14 17:34:38 +0200

    FEATURE: initialisation frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import tailwindcss from '@tailwindcss/vite'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+
+import "tailwindcss";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,6 +14,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
+       tailwindcss(),
       reactRefresh.configs.vite,
     ],
     languageOptions: {
